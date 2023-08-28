0e4723141a1ce9c0873338aa322d56e059cd05d8
jafar <jpyjafardarwesh@gmil.com>
2023-08-28 14:49:30 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
-function App() {
+export default function App() {
+  const [x,X]=useState([{}]);
+  useEffect(()=>{
+    xAPI()
+  },[])
+  const xAPI=()=>{
+    axios.get("https://jafardarwesh.github.io/my_api/data.json").then((resp)=>{
+      X(resp.data)
+      console.log('====================================');
+      console.log(resp.data);
+      console.log('====================================');
+    }).catch((error)=>console.log(error))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    {
+      x.map(i=>{
+        return <p>{i.id} -- {i.name}</p>
+      })
+    }
     </div>
-  );
+  )
 }
-
-export default App;
